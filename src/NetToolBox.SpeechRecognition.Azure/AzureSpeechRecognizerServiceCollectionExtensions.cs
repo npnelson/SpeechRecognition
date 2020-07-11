5ec9f15e8cdc674eb08ee3185dd5c5d5fd4aba00
npnelson <nnelson1@insight.rr.com>
2020-07-11 07:47:41 -0400

    better documentation and validation

--- src/NetToolBox.SpeechRecognition.Azure/AzureSpeechRecognizerServiceCollectionExtensions.cs
+++ src/NetToolBox.SpeechRecognition.Azure/AzureSpeechRecognizerServiceCollectionExtensions.cs
@@ -1,20 +1,36 @@
 ﻿using Microsoft.Extensions.Configuration;
 using NetToolBox.SpeechRecognition.Abstractions;
 using NetToolBox.SpeechRecognition.Azure;
+using System;
+using System.Linq;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class AzureSpeechRecognizerServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds a ISpeechRecognizer with Azure implementation
+        /// You must register options for AzureSpeechRecognizerSettings separately when using this overload
+        /// This overload will be deprecated once Azure Functions support ASPNet core style configuration https://github.com/Azure/azure-functions-host/issues/4761
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddAzureSpeechRecognizer(this IServiceCollection services)
         {
-            //TODO: Do some checking to make sure options have been registered            
+            var optionsRegistered = services.Any(x => x.ServiceType.GenericTypeArguments.Any(x => x == typeof(AzureSpeechRecognizerSettings)));
+            if (!optionsRegistered) throw new InvalidOperationException("You must register options for Azure SpeechRecognizerSettings");
             services.AddScoped<ISpeechRecognizer, AzureSpeechRecognizer>();
             return services;
         }
+        /// <summary>
+        /// Adds a ISpeechRecognizer with Azure implementation, configurationsection must contain values for AzureSpeechRecognizerSettings
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configurationSection"></param>
+        /// <returns></returns>
         public static IServiceCollection AddAzureSpeechRecognizer(this IServiceCollection services, IConfigurationSection configurationSection)
         {
-            //TODO: Do some checking to make sure configsection is valid
+
             services.Configure<AzureSpeechRecognizerSettings>(configurationSection);
             services.AddAzureSpeechRecognizer();
             return services;
